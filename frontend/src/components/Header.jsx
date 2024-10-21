adb2d87b8b93f8f77e30a85addddb52d627a9f62
Lakshay <lakshay.rv@gmail.com>
2024-10-21 14:55:02 -0400

    header component modified and specialitymenu component created

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -16,7 +16,7 @@ const Header = () => {
                 <img className='w-28' src={assets.group_profiles} alt="" />
                 <p>Simply browse through our extensive list of trusted doctors,<br className='hidden sm:block'/>schedule your appointment hassle-free.</p>
             </div>
-            <a href="" className='flex items-center gap-2 bg-white px-8 py-3 rounded-full text-gray-600 m-auto md:m-0 hover:scale-105 transition-all duration-300'>
+            <a href="#speciality" className='flex items-center gap-2 bg-white px-8 py-3 rounded-full text-gray-600 m-auto md:m-0 hover:scale-105 transition-all duration-300'>
                 Book Appointment <img className='w-3' src={assets.arrow_icon} alt="" />
             </a>
         </div>
